18d4489ef7562e9d4a361988fb43d7927b473ad8
Ken Chesshir <kchessh@gmail.com>
2022-08-13 12:35:39 -0500

    Add League3, change League2, and add script for getting expected wins

---
+++ Expected team wins.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import pprint
+import re
+import pandas as pd
+
+response = requests.get(f"https://www.sportsbettingdime.com/college-football/win-totals-best-odds/")
+
+text = response.text
+soup = BeautifulSoup(text, "html.parser")
+
+song_list = []
+teams_raw = str(soup.select(selector=".table-responsive"))
+
+teams = re.findall('(?:<td>)([\w\s\&\;-]*)(?:<\/td>)', teams_raw)
+wins = re.findall('(?:<td>)([0-9]+\.[0-9]+)(?: )', teams_raw)
+
+dict = dict(zip(teams, wins))
+print(dict)
+
+with open("Teams.txt", encoding='ISO-8859-1') as file:
+    text = file.read()
+    eligible_teams = text.split(",")
+    new_dict = {team: win for (team, win) in dict.items() if team in eligible_teams}
+
+print(new_dict)
+write_data = pd.DataFrame(new_dict, index=['wins', 'wins'])
+write_data.to_csv("Expected_wins.csv")
